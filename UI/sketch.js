0db6d3dada4d9e0ed15503e67d0460ac40703a5b
ItssJesse <159794466+ItssJesse@users.noreply.github.com>
2024-02-15 10:05:29 -0500

    UI dev1

--- UI/sketch.js
+++ UI/sketch.js
@@ -4,8 +4,7 @@ class UI {
   constructor() {
     this.isAccount = false;
 
-    this.loginPage = false;
-    this.hobbiesPage = true
+    this.loginPage = true;
     this.postsPage = false;
     if (this.isAccount) {
       this.loginPage = false;
@@ -14,12 +13,6 @@ class UI {
     this.messagingPage = false;
     this.makePostsPage = false;
     this.accountPage = false;
-
-    this.typing = 0;
-
-    this.username = "";
-    this.pet = "";
-    this.age = "";
   }
 
   update() {
@@ -27,60 +20,63 @@ class UI {
       this.runLogPage();
     }
     
-    if (this.hobbiesPage) {
-      this.runHobbiesPage();
-    }
-
     if (this.postsPage) {
       this.runPostsPage();
     }
-
-    this.runBottomMenu();
+    
+    this.runBottomMenu()
   }
-
-  runBottomMenu() {
+  
+  runBottomMenu()
+  {
     rectMode(CENTER);
-    noStroke();
-    fill(40);
-    rect(200, 575, 400, 50);
-
-    stroke(100, 180, 255);
-    strokeWeight(2);
-
-    if (mouseX > 180 && mouseX < 220 && mouseY > 560 && mouseY < 590) {
-      stroke(230, 240, 255);
-      strokeWeight(2);
-    } else {
-      stroke(100, 180, 255);
-      strokeWeight(2);
-    }
-    rect(200, 575, 35, 27, 5);
-    line(200, 568, 200, 582);
-    line(190, 575, 210, 575);
-
-    if (dist(mouseX, mouseY, 300, 575) < 15) {
-      stroke(230, 240, 255);
-      strokeWeight(2);
-    } else {
-      stroke(100, 180, 255);
-      strokeWeight(2);
-    }
-
-    ellipse(300, 570, 10, 10);
-    ellipse(300, 589, 20, 20);
-
-    noStroke();
-    rect(300, 595, 30, 15);
-
-    if (dist(mouseX, mouseY, 100, 575) < 15) {
-      stroke(230, 240, 255);
-      strokeWeight(2);
-    } else {
-      stroke(100, 180, 255);
-      strokeWeight(2);
-    }
-
-    triangle(105, 575, 90, 568, 90, 582);
+    noStroke()
+    fill(40)
+    rect(200, 575, 400, 50)
+    
+    stroke(100, 180, 255)
+    strokeWeight(2)
+    
+    if (mouseX > 180 && mouseX < 220 && mouseY > 560 && mouseY < 590)
+      {
+        stroke(230, 240, 255)
+        strokeWeight(2)
+      } else {
+        
+        stroke(100, 180, 255)
+        strokeWeight(2)
+      }
+    rect(200, 575, 35, 27, 5)
+    line(200, 568, 200, 582)
+    line(190, 575, 210, 575)
+    
+    if (dist(mouseX, mouseY, 300, 575) < 15)
+      {
+        stroke(230, 240, 255)
+        strokeWeight(2)
+      } else {
+        
+        stroke(100, 180, 255)
+        strokeWeight(2)
+      }
+    
+    ellipse(300, 570, 10, 10)
+    ellipse(300, 589, 20, 20)
+    
+    noStroke()
+    rect(300, 595, 30, 15)
+    
+    if (dist(mouseX, mouseY, 100, 575) < 15)
+      {
+        stroke(230, 240, 255)
+        strokeWeight(2)
+      } else {
+        
+        stroke(100, 180, 255)
+        strokeWeight(2)
+      }
+    
+    triangle(105, 575, 90, 568, 90, 582)
   }
 
   runLogPage() {
@@ -92,85 +88,42 @@ class UI {
     fill(200);
     textStyle(NORMAL);
     textSize(15);
-    text("Name", 200, 130);
+    text("Name (First and Last)", 200, 130);
 
     noFill();
-    stroke(150);
-    if (mouseX > 100 && mouseX < 300 && mouseY > 135 && mouseY < 165) {
-      fill(50);
-    }
-    if (this.typing == 1) {
-      stroke(190, 210, 220);
-      fill(50);
-    }
+    stroke(200);
     rect(200, 150, 200, 25, 5);
-
-    noStroke();
-    fill(255);
-    if (this.username != null) text(this.username, 200, 155);
-
-    textStyle(NORMAL);
-    textSize(15);
-    text("Pet Animal", 200, 230);
-
-    noFill();
-    stroke(150);
-    if (mouseX > 100 && mouseX < 300 && mouseY > 235 && mouseY < 265) {
-      fill(50);
-    }
-    if (this.typing == 2) {
-      stroke(190, 210, 220);
-      fill(50);
-    }
-    rect(200, 250, 200, 25, 5);
-
-    noStroke();
-    fill(255);
-    if (this.pet != null) text(this.pet, 200, 255);
 
     noStroke();
     fill(200);
     textStyle(NORMAL);
     textSize(15);
-    text("Age", 200, 330);
+    text("Pet Animal", 200, 230);
 
     noFill();
-    stroke(150);
-    if (mouseX > 100 && mouseX < 300 && mouseY > 335 && mouseY < 365) {
-      fill(50);
-    }
-    if (this.typing == 3) {
-      stroke(190, 210, 220);
-      fill(50);
-    }
-    rect(200, 350, 200, 25, 5);
+    stroke(200);
+    rect(200, 250, 200, 25, 5);
 
     noStroke();
-    fill(255);
-    if (this.age != null) text(this.age, 200, 355);
+    fill(200);
+    textStyle(NORMAL);
+    textSize(15);
+    text("other", 200, 330);
+
+    noFill();
+    stroke(200);
+    rect(200, 350, 200, 25, 5);
 
     if (mouseX > 100 && mouseX < 300 && mouseY > 430 && mouseY < 470) {
-      if (
-        mouseIsPressed == true &&
-        this.username != "" &&
-        this.pet != "" &&
-        this.age != ""
-      ) {
-        this.postsPage = true;
-        this.loginPage = false;
-      }
-
-      if (this.username == "" || this.pet == "" || this.age == "") {
-        noFill();
-        stroke(200, 150, 150);
-        rect(200, 450, 204, 44, 5);
-        fill(200, 100, 100);
-      } else {
-        noFill();
-        stroke(100, 255, 100);
-        rect(200, 450, 204, 44, 5);
-        fill(200);
-      }
+      if (mouseIsPressed == true)
+        {
+          this.postsPage = true
+          this.loginPage = false
+        }
+      noFill();
+      stroke(200);
+      rect(200, 450, 204, 44, 5);
+      fill(200);
     } else {
       fill(100, 180, 255);
       noStroke();
@@ -183,93 +136,33 @@ class UI {
     textSize(20);
     text("Create Account", 200, 456);
   }
-
-  addToKey(k) {
-    if (this.typing == 1 && textWidth(this.username) < 275) this.username += k;
-    if (this.typing == 2 && textWidth(this.pet) < 275) this.pet += k;
-    if (this.typing == 3 && textWidth(this.age) < 20 && key >= 0 && key <= 9)
-      this.age += k;
-  }
-
-  backspace() {
-    if (this.typing == 1) {
-      this.username = this.username.substr(0, this.username.length - 1);
-    }
-
-    if (this.typing == 2) {
-      this.pet = this.pet.substr(0, this.pet.length - 1);
-    }
-
-    if (this.typing == 3) {
-      this.age = this.age.substr(0, this.age.length - 1);
-    }
-  }
   
-  runHobbiesPage()
+  runPostsPage()
   {
     background(30)
-    
-    textAlign(CENTER)
-    textStyle(ITALIC)
-    noStroke()
-    fill(200)
-    textSize(20)
-    text("Pick your three favourite", 197, 50)
-    
-    
-    fill(20)
-    textSize(40)
-    textStyle(BOLD)
-    text("HOBBIES", 200, 95)
-    fill(100, 180, 255)
-    text("HOBBIES", 200, 90)
-    
-    
-    for (let i = 0; i < 2; i++)
-      {
-        for (let j = 0; j < 5; j++)
-          {
-            rectMode(CORNER)
-            noStroke()
-            fill(20)
-            rect(i*175+37.5, j * 75 + 160, 150, 50, 5)
-            fill(50)
-            rect(i*175+37.5, j * 75 + 150, 150, 50, 5)
-          }
-      }
   }
-
-  runPostsPage() {
-    background(30);
-  }
-
-  mouseRelease() {
-    if (this.loginPage) {
-      if (mouseX > 100 && mouseX < 300 && mouseY > 135 && mouseY < 165) {
-        this.typing = 1;
-        return;
-      }
-      if (mouseX > 100 && mouseX < 300 && mouseY > 235 && mouseY < 265) {
-        this.typing = 2;
-        return;
-      }
-      if (mouseX > 100 && mouseX < 300 && mouseY > 335 && mouseY < 365) {
-        this.typing = 3;
-        return;
-      }
-
-      this.typing = 0;
+  
+  updateText() {
+  let newText = this.value();
+  let lines = newText.split('\n');
+  let formattedText = '';
+  
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+    while (line.length > maxCharsPerLine) {
+      formattedText += line.substring(0, maxCharsPerLine) + '\n';
+      line = line.substring(maxCharsPerLine);
     }
-
-    //rect(200, 150, 200, 25, 5);
-    //rect(200, 250, 200, 25, 5);
-    //rect(200, 350, 200, 25, 5);
+    formattedText += line + '\n';
   }
+  
+  inputText = formattedText;
+}
 }
 
 function setup() {
   createCanvas(400, 600);
-  
+
   ui = new UI();
 }
 
@@ -277,18 +170,4 @@ function draw() {
   background(220);
 
   ui.update();
-}
-
-function keyTyped() {
-  if (key != "Enter") ui.addToKey(key);
-}
-
-function keyPressed() {
-  if (keyCode == BACKSPACE) {
-    ui.backspace();
-  }
-}
-
-function mouseReleased() {
-  ui.mouseRelease();
 }
