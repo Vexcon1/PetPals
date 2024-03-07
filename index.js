354f73593b52ce3d36caea13a60e138d006197a8
Vexcon1 <53382767+Vexcon1@users.noreply.github.com>
2024-03-07 09:45:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const StormDB = require("stormdb");
 
-// Serve static files in the public directory
+// start db with "./db.stormdb" storage location
+const engine = new StormDB.localFileEngine("./db.stormdb");
+const db = new StormDB(engine);
+
+const port = process.env.PORT || 3000;
+
+// set default db value if db is empty
+db.default({ users: [] });
+
 app.use(express.static('public'));
 
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('mouse', (data) => {
+    socket.broadcast.emit('mouse', data);
+  });
+
+  socket.on('database', (payload) => {
+    var { key, data, value } = payload;
+    console.log(payload);
+    console.log(key, data, value);
+    if (key === "set") {
+      db.set(`${data}`, value).save();
+    }
+    if (key === "get") {
+      let theData = db.get(`${data}`).value();
+      socket.emit('database', { key: 'get', data: theData });
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
 // Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
